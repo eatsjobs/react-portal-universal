900773dd89db7ca71e31f67f8053b4816f98c436
Nikita Lobachev <sanniassin@gmail.com>
2018-09-25 11:31:47 +0300

    Push lib into repository

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+export declare type ChildrenSelectorTuple = [any, string];
+export declare function createUniversalPortal(children: any, selector: string): any;
+export declare function flushUniversalPortals(): ChildrenSelectorTuple[];
+export declare function removeUniversalPortals(): void;
---
+++ lib/server.d.ts
@@ -0,0 +1,1 @@
+export declare function appendUniversalPortals(html: string): string;
